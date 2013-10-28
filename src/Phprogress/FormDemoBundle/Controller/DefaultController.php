d2f32e205911956b30538db68efb9d468c9c371d
Timon Schroeter <timon.schroeter@marketvalid.com>
2013-10-28 18:31:48 +0100

    Controller to demonstrate an exception.

--- src/Phprogress/FormDemoBundle/Controller/DefaultController.php
+++ src/Phprogress/FormDemoBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Phprogress\FormDemoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -15,5 +16,13 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+
+    /**
+     * @Route("/exception")
+     */
+    public function exceptionAction()
+    {
+        throw new NotFoundHttpException ('This Exception is thrown to simulate that some content has not been found.');
     }
 }
